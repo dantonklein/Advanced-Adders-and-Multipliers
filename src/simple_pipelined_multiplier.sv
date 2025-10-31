9caa88389fa50bec662b7a960def3560f3f1d8a3
Danton Klein <dantonmklein@gmail.com>
2025-10-31 03:36:25 -0400

    weenie

--- src/simple_pipelined_multiplier.sv
+++ src/simple_pipelined_multiplier.sv
@@ -1,10 +1,12 @@
 //this will be temporarily used until the 28 bit dadda is made
-module Multiplier_28bit_pipelined (
+module multiplier_delayed #(
+    parameter int WIDTH = 27
+    )(
     input logic clk, rst, 
-    input logic[27:0] in1, in2,
-    output logic[55:0] out
+    input logic[WIDTH-1:0] in1, in2,
+    output logic[2*WIDTH-1:0] out
 );
-logic[55:0] s2_mult_out;
+logic[2*WIDTH-1:0] s2_mult_out;
 //fpga would optimize across 2 clock cycles
 always_ff @(posedge clk or posedge rst) begin
     if(rst) begin
